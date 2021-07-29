3134a4cc26f638082956d23702172b01dd311a03
Astronom-Oort <75095625+Astronom-Oort@users.noreply.github.com>
2021-07-29 23:09:34 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,19 +1,18 @@
 import math
- 
 a = float(input("a = "))
 b = float(input("b = "))
 c = float(input("c = "))
  
-Discriminant = b ** 2 - 4 * a * c
+D = b ** 2 - 4 * a * c
 print("Дискриминант равен -->",Discriminant)
  
 if Discriminant > 0:
-    x1 = (-b + math.sqrt(Discriminant)) / (2 * a)
-    x2 = (-b - math.sqrt(Discriminant)) / (2 * a)
+    x1 = (-b + math.sqrt(D)) / (2 * a)
+    x2 = (-b - math.sqrt(D)) / (2 * a)
     print("x1 -->", x1, "\n", "x2 --> ", x2    )
-elif Discriminant == 0:
+elif not D:
     x = -b / (2 * a)
-    print("x = -->", Discriminant)
+    print("x = -->", D)
 else:
     print("Уравнение корней не имеет  \n",
           "Ответ: ∅ ")
