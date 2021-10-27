031b6a8878ecb77fb03d7c13a221b3ecb2a9d28d
Astronom-Oort <75095625+Astronom-Oort@users.noreply.github.com>
2021-10-27 22:47:47 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -4,12 +4,12 @@ b = float(input("b = "))
 c = float(input("c = "))
  
 D = b ** 2 - 4 * a * c
-print("Дискриминант равен -->",Discriminant)
+print(f"Дискриминант равен -->{Discriminant}")
  
 if Discriminant > 0:
     x1 = (-b + math.sqrt(D)) / (2 * a)
     x2 = (-b - math.sqrt(D)) / (2 * a)
-    print("x1 -->", x1, "\n", "x2 --> ", x2    )
+    print("x1 -->", x1, "\n", "x2 --> ", x2)
 elif not D:
     x = -b / (2 * a)
     print("x = -->", D)
