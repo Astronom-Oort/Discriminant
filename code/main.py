f3d91a4153198d63d04c44f30d4f79903a279d88
Astronom-Oort <berrydohn@gmail.com>
2022-06-09 09:41:42 +0300

    Update main.py

--- code/main.py
+++ code/main.py
@@ -1,11 +1,14 @@
 import math
-a = float(input("a = "))
-b = float(input("b = "))
-c = float(input("c = "))
- 
+from tokenize import Number
+
+from numpy import number
+a = int(input("a = "))
+b = int(input("b = "))
+c = int(input("c = "))
+
 D = b ** 2 - 4 * a * c
-print(f"Дискриминант равен --> {D}")
- 
+print(f"The discriminant is equal to --> {D}")
+
 if D > 0:
     x1 = (-b + math.sqrt(D)) / (2 * a)
     x2 = (-b - math.sqrt(D)) / (2 * a)
@@ -14,5 +17,5 @@ elif D == 0:
     x = -b / (2 * a)
     print("x = -->", x)
 else:
-    print("Уравнение корней не имеет  \n",
-          "Ответ: ∅ ")
+    print("The equation has no roots  \n",
+          "Output: ∅ ")
